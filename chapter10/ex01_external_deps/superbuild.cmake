9364726c3f61408f6d6f18a76902cc6a64fc67f8
ClausKlein <claus.klein@arcormail.de>
2024-09-09 11:01:11 +0200

    Change clang-format config file
    Use "line_width": 85

--- chapter10/ex01_external_deps/superbuild.cmake
+++ chapter10/ex01_external_deps/superbuild.cmake
@@ -2,8 +2,8 @@
 #
 # SPDX-License-Identifier: MIT
 
-# We will utilize the FetchContent module for satisfying the google benchmark
-# and google test dependencies.
+# We will utilize the FetchContent module for satisfying the google benchmark and
+# google test dependencies.
 include(FetchContent)
 
 message(
@@ -19,28 +19,24 @@ fetchcontent_declare(
     GIT_TAG v1.6.1
 )
 
-# Declare google test dependency details. We will explicitly require 1.10.0
-# version this time.
+# Declare google test dependency details. We will explicitly require 1.10.0 version
+# this time.
 fetchcontent_declare(
     GTest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG release-1.11.0
 )
 
-# Since Gtest 1.14 has the minimum required C++ standard set to 14 this is
-# enforced here
+# Since Gtest 1.14 has the minimum required C++ standard set to 14 this is enforced
+# here
 
-# Declaring dependencies via FetchContent_Declare does not make them
-# "available", meaning they will not be downloaded nor be installed until we
-# call FetchContent_MakeAvailable() or FetchContent_Populate().
+# Declaring dependencies via FetchContent_Declare does not make them "available",
+# meaning they will not be downloaded nor be installed until we call
+# FetchContent_MakeAvailable() or FetchContent_Populate().
 
 fetchcontent_makeavailable(GTest benchmark)
-# To make target names compatible with find_package(...), so we can switch
-# between superbuild and find_package_(...) easily.
-add_library(
-    GTest::Main
-    ALIAS
-    gtest_main
-)
+# To make target names compatible with find_package(...), so we can switch between
+# superbuild and find_package_(...) easily.
+add_library(GTest::Main ALIAS gtest_main)
 # Google benchmark already defines benchmark::benchmark target, so it is not
 # necessary to define an alias for it.
